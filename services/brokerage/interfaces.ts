b74c90caae2bf20c00cc65efd81f836ee6ad1063
jlancaster7 <lancaster.joshua.c@gmail.com>
2022-06-30 23:02:50 -0400

    Created the TP Trans and Port tables and such

--- services/brokerage/interfaces.ts
+++ services/brokerage/interfaces.ts
@@ -97,31 +97,107 @@ export type RealizefiAccountPosition = {
     securityPrimaryExchange: string
 }
 
-export type TradingPostHolding = {}
+export type TradingPostHoldingTable = {
+    id: string
+    account_id: string
+    security_id: string
+    security_type: SecurityType
+    institution_price: number
+    institution_as_of: DateTime | null
+    institution_value: number
+    costBasis: number | null
+    quantity: number
+    currency: string
+    // Should we put things like 'industry' on this table? I know we could join it on the securities table, but could then allow people to assign a custom industy 
+    // to a security if we don't have one for it.. or they want to change it.
+    // But then I guess maybe we should have a custom industry table so that if they trade in and out of it, it persists 
+}
 
-export type TradingPostTransaction = {}
+export type TradingPostCustomIndustryTable = {
+    id: string
+    user_id: string
+    security_id: string
+    industry: string
+}
 
-export type TradingPostAccount = {}
+export type TradingPostTransactionTable = {
+    id: string
+    account_id: string
+    security_id: string
+    security_type: SecurityType
+    date: DateTime
+    quantity: number
+    price: number
+    amount: number
+    fees: number | null
+    type: InvestmentTransactionType
+    currency: string
+}
 
-export type TradingPostPortfolioSummary = {
-    id: number
+export enum SecurityType {
+    equity = "equity",
+    option = "option",
+    index = "index",
+    mutualFund = "mutualFund",
+    cashEquivalent = "cashEquivalent", // A money market fund would be an example of a cash equivalent
+    fixedIncome = "fixedIncome",
+    currency = "currency"
+}
+
+export enum InvestmentTransactionType {
+    buy = "buy",
+    sell = "sell",
+    short = "short",
+    cover = "cover",
+    cancel = "cancel",
+    fee = "fee",
+    cash = "cash",
+    transfer = "transfer",
+    dividendOrInerest = "dividendOrInterest"
+}
+
+export type TradingPostAccountTable = {
+    id: string // account_id else where
+    broker_name: string
+    mask: string | null
+    name: string
+    official_name: string | null
+    type: string // Margin or Cash Account
+    subtype: string | null
+}
+
+export type TradingPostPortfolioSummaryTable = {
+    portfolio_id: number 
     beta: number
     sharpe: number
-    etc: number
+    industry_allocation: {[key: string]: number}
+    exposure: {
+        long: number
+        short: number
+        net: number
+        gross: number
+    }
     default_benchmark_id: number // References securities table
+}
+
+export type TradingPostPortfolioTable = {
+    id: string // portfolio_id else where
+    user_id: string
+    account_ids: string[]
 }
 
 export type BenchmarkHoldingPeriodReturnTable = {
     id: number
     security_id: number
-    price: number
     timestamp: DateTime
+    return: number
 }
 
 export type PortfolioHoldingPeriodReturnTable = {
     id: number
     portfolio_id: number | null
     timestamp: DateTime
+    return: number
 }
 
 
