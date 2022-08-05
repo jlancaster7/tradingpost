aa068e555268fc62897b1c41db6b81b99c2253af
jlancaster7 <lancaster.joshua.c@gmail.com>
2022-08-05 11:17:58 -0400

    clean up to push

--- common/brokerage/interfaces.js
+++ common/brokerage/interfaces.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.InvestmentTransactionType = exports.SecurityType = void 0;
@@ -26,32 +25,3 @@ var InvestmentTransactionType;
     InvestmentTransactionType["dividendOrInterest"] = "dividendOrInterest";
 })(InvestmentTransactionType = exports.InvestmentTransactionType || (exports.InvestmentTransactionType = {}));
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW50ZXJmYWNlcy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImludGVyZmFjZXMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBdWJBLElBQVksWUFTWDtBQVRELFdBQVksWUFBWTtJQUNwQixpQ0FBaUIsQ0FBQTtJQUNqQixpQ0FBaUIsQ0FBQTtJQUNqQiwrQkFBZSxDQUFBO0lBQ2YseUNBQXlCLENBQUE7SUFDekIsaURBQWlDLENBQUE7SUFDakMsMkNBQTJCLENBQUE7SUFDM0IscUNBQXFCLENBQUE7SUFDckIsbUNBQW1CLENBQUE7QUFDdkIsQ0FBQyxFQVRXLFlBQVksR0FBWixvQkFBWSxLQUFaLG9CQUFZLFFBU3ZCO0FBRUQsSUFBWSx5QkFVWDtBQVZELFdBQVkseUJBQXlCO0lBQ2pDLHdDQUFXLENBQUE7SUFDWCwwQ0FBYSxDQUFBO0lBQ2IsNENBQWUsQ0FBQTtJQUNmLDRDQUFlLENBQUE7SUFDZiw4Q0FBaUIsQ0FBQTtJQUNqQix3Q0FBVyxDQUFBO0lBQ1gsMENBQWEsQ0FBQTtJQUNiLGtEQUFxQixDQUFBO0lBQ3JCLHNFQUF5QyxDQUFBO0FBQzdDLENBQUMsRUFWVyx5QkFBeUIsR0FBekIsaUNBQXlCLEtBQXpCLGlDQUF5QixRQVVwQyJ9
-=======
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.InvestmentTransactionType = exports.SecurityType = void 0;
-var SecurityType;
-(function (SecurityType) {
-    SecurityType["equity"] = "equity";
-    SecurityType["option"] = "option";
-    SecurityType["index"] = "index";
-    SecurityType["mutualFund"] = "mutualFund";
-    SecurityType["cashEquivalent"] = "cashEquivalent";
-    SecurityType["fixedIncome"] = "fixedIncome";
-    SecurityType["currency"] = "currency";
-    SecurityType["unknown"] = "unknown";
-})(SecurityType = exports.SecurityType || (exports.SecurityType = {}));
-var InvestmentTransactionType;
-(function (InvestmentTransactionType) {
-    InvestmentTransactionType["buy"] = "buy";
-    InvestmentTransactionType["sell"] = "sell";
-    InvestmentTransactionType["short"] = "short";
-    InvestmentTransactionType["cover"] = "cover";
-    InvestmentTransactionType["cancel"] = "cancel";
-    InvestmentTransactionType["fee"] = "fee";
-    InvestmentTransactionType["cash"] = "cash";
-    InvestmentTransactionType["transfer"] = "transfer";
-    InvestmentTransactionType["dividendOrInterest"] = "dividendOrInterest";
-})(InvestmentTransactionType = exports.InvestmentTransactionType || (exports.InvestmentTransactionType = {}));
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW50ZXJmYWNlcy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImludGVyZmFjZXMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBcWJBLElBQVksWUFTWDtBQVRELFdBQVksWUFBWTtJQUNwQixpQ0FBaUIsQ0FBQTtJQUNqQixpQ0FBaUIsQ0FBQTtJQUNqQiwrQkFBZSxDQUFBO0lBQ2YseUNBQXlCLENBQUE7SUFDekIsaURBQWlDLENBQUE7SUFDakMsMkNBQTJCLENBQUE7SUFDM0IscUNBQXFCLENBQUE7SUFDckIsbUNBQW1CLENBQUE7QUFDdkIsQ0FBQyxFQVRXLFlBQVksR0FBWixvQkFBWSxLQUFaLG9CQUFZLFFBU3ZCO0FBRUQsSUFBWSx5QkFVWDtBQVZELFdBQVkseUJBQXlCO0lBQ2pDLHdDQUFXLENBQUE7SUFDWCwwQ0FBYSxDQUFBO0lBQ2IsNENBQWUsQ0FBQTtJQUNmLDRDQUFlLENBQUE7SUFDZiw4Q0FBaUIsQ0FBQTtJQUNqQix3Q0FBVyxDQUFBO0lBQ1gsMENBQWEsQ0FBQTtJQUNiLGtEQUFxQixDQUFBO0lBQ3JCLHNFQUF5QyxDQUFBO0FBQzdDLENBQUMsRUFWVyx5QkFBeUIsR0FBekIsaUNBQXlCLEtBQXpCLGlDQUF5QixRQVVwQyJ9
->>>>>>> 316cfbb83a9338125b56c7e1330be3d41cb710b7
