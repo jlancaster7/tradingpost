09de5de686cb3de0258ed98fdb67e385176ad461
David Bozentka <djbozentka@gmail.com>
2022-09-12 09:29:51 -0700

    Notifications

--- common/api/entities/apis/extensions.ts
+++ common/api/entities/apis/extensions.ts
@@ -4,6 +4,7 @@ export const Bookmark = Dummy
 export const Comment = Dummy
 export const PlatformClaim = Dummy
 import * as Post from '../extensions/Post'
+export * as Notification from "../extensions/Notification";
 import * as Subscriber from '../extensions/Subscriber'
 import * as Subscription from '../extensions/Subscription'
 export const Trade = Dummy
