ec0b495c7b0c96d566bfdada9791c3c0f09b9fdd
David Bozentka <djbozentka@gmail.com>
2022-10-12 07:29:18 -0700

    Removed ttt

--- app/ttt/TradingPost/components/HtmlView.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { View, Text } from "react-native"
-import { HtmlViewProps } from "./HtmlView.interface"
-import { WebView } from 'react-native-webview'
-
-export const HtmlView = (props: HtmlViewProps) => {
-    return <WebView style={props.style} source={props.isUrl ? { uri: props.children } : { html: props.children }} />
-}
--- app/ttt/TradingPost/components/NavHandler.tsx
+++
@@ -1,8 +0,0 @@
-import React, { PropsWithChildren } from "react"
-
-export const NavHandler: React.FC<PropsWithChildren<{}>> = (props) => {
-    //TODO: abstract navigation based on deep linking
-    return <>
-        {props.children}
-    </>
-}
