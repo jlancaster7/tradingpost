811d3873938d2904f8b387b9d7a4efae725e3cf9
David Bozentka <djbozentka@gmail.com>
2022-10-27 13:14:55 -0400

    Entitlement update

--- app/vvv/TradingPostExpo/App.tsx
+++ app/vvv/TradingPostExpo/App.tsx
@@ -1,58 +1,56 @@
-import { configApi } from '@tradingpost/common/api/entities/static/EntityApiBase'
-import Constants, { AppOwnership } from 'expo-constants'
-import { useURL } from 'expo-linking'
-import { parse } from 'url'
+import {configApi} from '@tradingpost/common/api/entities/static/EntityApiBase'
+import Constants, {AppOwnership} from 'expo-constants'
+import {useURL} from 'expo-linking'
+import {parse} from 'url'
 
 if (!__DEV__) {
-  configApi({
-    apiBaseUrl: "https://api.tradingpostapp.com"
-  })
-}
-else if (__DEV__ && (AppOwnership.Expo === Constants.appOwnership || AppOwnership.Standalone === Constants.appOwnership || !Constants.appOwnership)) {
-
-  //  console.log("BUNLDE URL IS " + Constants.manifest?.hostUri);
-  // console.log("OTHER :" + console.log(JSON.stringify(Constants)));
-
-  if (Constants.manifest?.hostUri)
     configApi({
-      apiBaseUrl: `http://${Constants.manifest?.hostUri?.split(":")[0]}:8082`
+        apiBaseUrl: "https://api.tradingpostapp.com"
     })
-  else
-    //manual ip for api server... have been trying to find a way to avoid this...
-    configApi({
-      apiBaseUrl: `http://10.229.201.27:8082`
-    })
+} else if (__DEV__ && (AppOwnership.Expo === Constants.appOwnership || AppOwnership.Standalone === Constants.appOwnership || !Constants.appOwnership)) {
+
+    //  console.log("BUNLDE URL IS " + Constants.manifest?.hostUri);
+    // console.log("OTHER :" + console.log(JSON.stringify(Constants)));
+
+    if (Constants.manifest?.hostUri)
+        configApi({
+            apiBaseUrl: `http://${Constants.manifest?.hostUri?.split(":")[0]}:8082`
+        })
+    else
+        //manual ip for api server... have been trying to find a way to avoid this...
+        configApi({
+            apiBaseUrl: `http://127.0.0.1:8082/`
+        })
 
 
+} else {
+    console.log("THIS IS A DIFFERENT CONDITION DEV: " + __DEV__ + " OWNER: " + Constants.appOwnership)
 }
-else {
-  console.log("THIS IS A DIFFERENT CONDITION DEV: " + __DEV__ + " OWNER: " + Constants.appOwnership)
-}
 
 
-
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 //import { SafeAreaProvider } from 'react-native-safe-area-context';
 //import { Colors } from 'react-native-ui-lib';
-import { Logs } from 'expo'
+import {Logs} from 'expo'
+
 Logs.enableExpoCliLogging()
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import { LogBox, Platform, Text } from 'react-native'
+import {LogBox, Platform, Text} from 'react-native'
 import WelcomeScreen from './screens/WelcomeScreen';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Button, IconRegistry } from '@ui-kitten/components';
+import {ApplicationProvider, Layout, Button, IconRegistry} from '@ui-kitten/components';
 import theme from "./theme-light.json"; // <-- Import app theme
-import { ToastProvider } from 'react-native-toast-notifications';
-import { setValue, useData } from './lds'
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { NavigationProp, useLinkTo, useNavigation } from '@react-navigation/native';
+import {ToastProvider} from 'react-native-toast-notifications';
+import {setValue, useData} from './lds'
+import React, {useCallback, useEffect, useRef, useState} from 'react';
+import {NavigationProp, useLinkTo, useNavigation} from '@react-navigation/native';
 
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { useAppUser } from './Authentication';
-import { getSecurityList } from './SecurityList'
-import { Api } from "@tradingpost/common/api/index";
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
+import {useAppUser} from './Authentication';
+import {getSecurityList} from './SecurityList'
+import {Api} from "@tradingpost/common/api/index";
 
 // Colors.loadColors({
 //   primary: '#11146F',
@@ -68,89 +66,88 @@ import { Api } from "@tradingpost/common/api/index";
 
 
 export default function App() {
-  const { isLoadingComplete } = useCachedResources();
+    const {isLoadingComplete} = useCachedResources();
 
-  const colorScheme = useColorScheme();
-  const { appUser, signIn } = useAppUser();
-  const { value: authToken, setValue: setAuthToken } = useData("authToken");
+    const colorScheme = useColorScheme();
+    const {appUser, signIn} = useAppUser();
+    const {value: authToken, setValue: setAuthToken} = useData("authToken");
 
-  //try auth signin
-  useEffect(() => {
-    if (authToken) {
-      signIn("", authToken);
-    }
-  }, [Boolean(authToken)])
-
-
-  useEffect(() => {
-    if (isLoadingComplete) {
-
-      getSecurityList();
-      //needed to add a delay to this request. It is not working if done prior to this
-      //setTimeout(() => {
-      //console.log("TRYING TO LOAD SECURITIES");
-
-      //}, 4000)
-
-    }
-  }, [isLoadingComplete])
-  const url = useURL();
-  console.log(url);
-  const linkTo = useLinkTo();
-  const ltRef = useRef(linkTo);
-  ltRef.current = linkTo;
-  const [urlToGoTo, setUrlToGoTo] = useState("");
-  if (__DEV__ && url) {
-
-    const urlParsed = parse(url, true);
-
-    //com.tradingpostapp://expo-development-client/?url=http%3A%2F%2F10.0.0.94%3A8081 need to parse and set the api url the first run through
-    if (urlParsed.hostname === "expo-development-client") {
-      configApi({ apiBaseUrl: "http://" + (urlParsed.query["url"] as string)?.split(":")[1] + ":8082" });
-    }
-    else {
-      if (!urlToGoTo) {
-        console.log("GOING TO URL :::::::: " + urlToGoTo);
-        setUrlToGoTo(url);
-        
-      }
-    }
-    // else if (urlParsed.hostname?.toString() === "m.tradingpostapp.com" && !urlToGoTo) {
-    //   console.log("Sending you to");
-    //   setUrlToGoTo(url);
-    // }
-  }
-
-  // useEffect(() => {
-  //   console.log("Link To Value " + urlToGoTo);
-  //   try {
-  //     if (urlToGoTo) {
-  //       setTimeout(() => linkTo(urlToGoTo), 3000);
-  //     }
-  //   }
-  //   catch (ex) {
-
-  //     console.error(ex);
-  //   }
-  // }, [linkTo, urlToGoTo])
-
-  // if (!isLoadingComplete) {
-  //   return null;
-  // }
-
-
-  return <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
-    <IconRegistry icons={EvaIconsPack} />
-    <ToastProvider>
-      <Navigation whenReady={() => {
-        console.log("Nav Container says its ready");
-        if (urlToGoTo) {
-          linkTo(urlToGoTo)
+    //try auth signin
+    useEffect(() => {
+        if (authToken) {
+            signIn("", authToken);
         }
-      }} colorScheme={colorScheme} />
-      <StatusBar />
-    </ToastProvider>
-  </ApplicationProvider>
+    }, [Boolean(authToken)])
+
+
+    useEffect(() => {
+        if (isLoadingComplete) {
+
+            getSecurityList();
+            //needed to add a delay to this request. It is not working if done prior to this
+            //setTimeout(() => {
+            //console.log("TRYING TO LOAD SECURITIES");
+
+            //}, 4000)
+
+        }
+    }, [isLoadingComplete])
+    const url = useURL();
+    console.log(url);
+    const linkTo = useLinkTo();
+    const ltRef = useRef(linkTo);
+    ltRef.current = linkTo;
+    const [urlToGoTo, setUrlToGoTo] = useState("");
+    if (__DEV__ && url) {
+
+        const urlParsed = parse(url, true);
+
+        //com.tradingpostapp://expo-development-client/?url=http%3A%2F%2F10.0.0.94%3A8081 need to parse and set the api url the first run through
+        if (urlParsed.hostname === "expo-development-client") {
+            configApi({apiBaseUrl: "http://" + (urlParsed.query["url"] as string)?.split(":")[1] + ":8082"});
+        } else {
+            if (!urlToGoTo) {
+                console.log("GOING TO URL :::::::: " + urlToGoTo);
+                setUrlToGoTo(url);
+
+            }
+        }
+        // else if (urlParsed.hostname?.toString() === "m.tradingpostapp.com" && !urlToGoTo) {
+        //   console.log("Sending you to");
+        //   setUrlToGoTo(url);
+        // }
+    }
+
+    // useEffect(() => {
+    //   console.log("Link To Value " + urlToGoTo);
+    //   try {
+    //     if (urlToGoTo) {
+    //       setTimeout(() => linkTo(urlToGoTo), 3000);
+    //     }
+    //   }
+    //   catch (ex) {
+
+    //     console.error(ex);
+    //   }
+    // }, [linkTo, urlToGoTo])
+
+    // if (!isLoadingComplete) {
+    //   return null;
+    // }
+
+
+    return <ApplicationProvider {...eva} theme={{...eva.light, ...theme}}>
+        <IconRegistry icons={EvaIconsPack}/>
+        <ToastProvider>
+            <Navigation whenReady={() => {
+                console.log("Nav Container says its ready");
+                if (urlToGoTo) {
+                    linkTo(urlToGoTo)
+                }
+            }} colorScheme={colorScheme}/>
+            <StatusBar/>
+        </ToastProvider>
+    </ApplicationProvider>
 }
 
 
--- app/vvv/TradingPostExpo/screens/WelcomeScreen.tsx
+++ app/vvv/TradingPostExpo/screens/WelcomeScreen.tsx
@@ -290,7 +290,7 @@ export default ({ navigation }: RootStackScreenProps<"Root">) => {
 
         <Animated.Text style={[bannerText, {
             opacity: !selectedIndex ? 1 : opacityAnim
-        }]}> {!selectedIndex ? "Welcome to the team!" : "Hey... Welcome Back!"}</Animated.Text>
+        }]}> {!selectedIndex ? "Welcome to the team!" : "Hey!!!!!... Welcome Back!"}</Animated.Text>
         <LoginButtons
             createAccountProps={{
                 onPress: () => {
