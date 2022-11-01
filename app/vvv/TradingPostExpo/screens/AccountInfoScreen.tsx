aa5753881bf150805e9d5ba0572021a8fe3955ba
jlancaster7 <lancaster.joshua.c@gmail.com>
2022-11-01 15:45:57 -0400

    Move 'Your Content' into a component

--- app/vvv/TradingPostExpo/screens/AccountInfoScreen.tsx
+++ app/vvv/TradingPostExpo/screens/AccountInfoScreen.tsx
@@ -26,13 +26,14 @@ import { KeyboardAvoidingInput } from "../components/KeyboardAvoidingInput";
 import { Picker } from "../components/Picker";
 import { ProfileButton } from "../components/ProfileButton";
 import { Slider } from "../components/Slider";
-import { HandleButton } from "./create_account/YourContentSection";
+//import { HandleButton } from "./create_account/YourContentSection";
 import { useTwitterAuth } from "../utils/third-party/twitter";
 import { social } from "../images";
 import { AppColors } from "../constants/Colors";
 import { useToast } from "react-native-toast-notifications";
 import { AccountSettingsScreen } from './AccountSettingsScreen';
 import { useGoogleAuth } from "../utils/third-party/youtube";
+import { YourContentComponent } from "../components/YourContentComponent";
 
 export function AccountInfoScreen() {
     const { appUser, authToken, signIn } = useAppUser(),
@@ -43,6 +44,7 @@ export function AccountInfoScreen() {
     //########################################################################################################################
     //Logic for the 'Your Content Tab
     //########################################################################################################################
+    /*
     const [inputMessage, setInputMessage] = useState(''),
         [inputPlatform, setInputPlatform] = useState(''),
         [inputValue, setInputValue] = useState('');
@@ -50,14 +52,6 @@ export function AccountInfoScreen() {
 
     const getTwitterToken = useTwitterAuth();
     const getGoogleToken = useGoogleAuth();
-    //let twitterHandle: any;
-    //let setTwitterHandle: any;
-    //let substackUsername: any;
-    //let setsubstackUsername: any;
-    //let spotifyShow: any;
-    //let setSpotifyShow: any;
-    //let youtubeChannel: any;
-    //let setYoutubeChannel: any;
 
     let userClaims = appUser?.claims;
     
@@ -65,21 +59,6 @@ export function AccountInfoScreen() {
     let [substackUsername, setsubstackUsername] = useState(userClaims ? userClaims.find(c => c.platform === "substack")?.claims.handle : '')
     let [spotifyShow, setSpotifyShow] = useState(userClaims ? userClaims.find(c => c.platform === "spotify")?.claims.handle : '')
     let [youtubeChannel, setYoutubeChannel] = useState(userClaims ? userClaims.find(c => c.platform === "youtube")?.claims.handle : '')
-    
-    //TODO: we need to fix this... it could theoretically be very problematic.... react cant tell because 
-    /*
-    if (appUser?.claims) {
-        [twitterHandle, setTwitterHandle] = useState(appUser?.claims.find(c => c.platform === "twitter")?.claims?.handle);
-        [substackUsername, setsubstackUsername] = useState(appUser?.claims.find(c => c.platform === "substack")?.claims?.handle);
-        [spotifyShow, setSpotifyShow] = useState(appUser?.claims.find(c => c.platform === "spotify")?.claims?.handle);
-        [youtubeChannel, setYoutubeChannel] = useState(appUser?.claims.find(c => c.platform === "youtube")?.claims?.handle);
-    }
-    else {
-        [twitterHandle, setTwitterHandle] = useState('');
-        [substackUsername, setsubstackUsername] = useState('');
-        [spotifyShow, setSpotifyShow] = useState('');
-        [youtubeChannel, setYoutubeChannel] = useState('');
-    }
     */
     //########################################################################################################################
 
@@ -371,75 +350,7 @@ ${value.security_name}`,
                     </ElevatedSection>
                 </Tab>
                 <Tab title={'Your Content'} style={{ backgroundColor: AppColors.background, borderColor: AppColors.background }}>
-                    <ElevatedSection title="" style={{ padding: 5 }}>
-                        <Text style={[thinBannerText, { fontSize: fonts.medium, lineHeight: fonts.medium * 1.5 }]}>TradingPost aggregates content across several social platforms.</Text>
-                        <View style={{ flexDirection: "row", marginHorizontal: sizes.rem2, marginBottom: sizes.rem1 }}>
-                            <HandleButton
-                                title={twitterHandle}
-                                icon={social.TwitterLogo}
-                                onPress={() => {
-                                    getTwitterToken().then((handle) => {
-                                        setTwitterHandle(handle)
-                                    })
-                                }}
-                                iconPadd={sizes.rem1}
-                            />
-                            <HandleButton title={youtubeChannel} icon={social.YouTubeLogo}
-                                iconPadd={sizes.rem1}
-                                onPress={() => {
-                                    getGoogleToken().then((handle) => {
-                                        setYoutubeChannel(handle)
-                                    })
-                                    setInputPlatform('youtube')
-                                    //await Api.User.extensions.linkSocialAccount({platform: 'substack', platform_idenifier: ''}) ;  
-                                }}
-                            />
-                        </View>
-                        <View style={{ flexDirection: "row", marginHorizontal: sizes.rem2, marginBottom: sizes.rem1 }}>
-                            <HandleButton title={substackUsername} icon={social.SubstackLogo}
-                                iconPadd={sizes.rem1}
-                                currentColor={socialStyling.substackColor}
-                                onPress={() => {
-                                    setInputMessage('Please enter your Substack username as it appears in your profile URL');
-                                    setInputPlatform('substack')
-                                    //await Api.User.extensions.linkSocialAccount({platform: 'substack', platform_idenifier: ''}) ;  
-                                }}
-                            />
-                            <HandleButton title={spotifyShow} icon={social.SpotifyLogo}
-                                iconPadd={sizes.rem0_5}
-                                onPress={() => {
-                                    setInputMessage("Please enter your Spotify Podcast's Profile URL.\nIn the Spotify App, this can be found by going to your Podcasts's Profile Page, clicking the 'Share' Icon and clicking 'Copy Link'.");
-                                    setInputPlatform('spotify')
-                                    //await Api.User.extensions.linkSocialAccount({platform: 'substack', platform_idenifier: ''}) ;  
-                                }}
-                            />
-                        </View>
-                        <Text style={[thinBannerText, { fontSize: fonts.medium, lineHeight: fonts.medium * 1.5 }]}>Sign in to your account(s) above to claim or add your content.</Text>
-                        <View style={inputPlatform === '' ? { display: 'none' } : { display: 'flex' }}>
-                            <Text>
-                                {inputMessage}
-                            </Text>
-                            <KeyboardAvoidingInput
-                                placeholder={'Enter here'}
-                                value={inputValue}
-                                setValue={setInputValue}
-                                displayButton={true}
-                                numLines={2}
-                                rightIcon={(props: any) => <Icon
-                                    fill={AppColors.secondary}
-                                    name="plus-square" height={35} width={35} style={{ height: 35, width: 35, padding: 0, alignContent: 'center' }} />}
-
-                                onClick={async (r: any, s: any, t: any) => {
-                                    const result = await Api.User.extensions.linkSocialAccount({ platform: inputPlatform, platform_idenifier: r });
-                                    if (result === '') {
-                                        toast.show(`${inputPlatform} Account Link Failed. Please try again or email contact@tradingpostapp.com for assistance.`)
-                                    }
-                                    s('')
-                                    authToken ? await signIn("", authToken) : {};
-                                }}
-                            />
-                        </View>
-                    </ElevatedSection>
+                    <YourContentComponent />
                 </Tab>
                 <Tab title={'Advanced'} style={{ backgroundColor: AppColors.background, borderColor: AppColors.background }}>
                     <ElevatedSection title="" style={{ padding: 5 }}>
