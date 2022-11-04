7c5fe0a04eec3716963d2ce25d4f094cf2319640
Levert James <ljames@ospark.com>
2022-11-04 09:29:55 -0400

    partial code cleanup 2

--- common/api/entities/interfaces/index.ts
+++ common/api/entities/interfaces/index.ts
@@ -1,88 +1,88 @@
 import * as Statics from '../static/interfaces'
 
 export interface IAlertList {
-        id: number,
+    id: number,
     data: any,
     type: string,
     user_id: string
-    };
+};
 
 export interface IAlertGet {
-    
-    };
+
+};
 
 export interface IBookmarkList {
-        user_id: string,
+    user_id: string,
     post_id: string,
     id: number
-    };
+};
 
 export interface IBookmarkGet {
-        id: number,
+    id: number,
     post_id: string,
     user_id: string
-    };
+};
 
 export interface ICommentList {
-        id: number,
+    id: number,
     related_type: string,
     related_id: string,
     comment: string,
     user_id: string
-    };
+};
 
 export interface ICommentGet {
-        comment: string,
+    comment: string,
     id: number,
     related_type: string,
     related_id: string,
     user_id: string
-    };
+};
 
 export interface ICommentInsert {
-        related_type: string,
+    related_type: string,
     related_id: string,
     comment: string,
     user_id: string
-    };
+};
 
 export interface IPlatformClaimList {
-        platform: string,
+    platform: string,
     claims?: any,
     id: number,
     user_id: string,
     platform_user_id: string
-    };
+};
 
 export interface IPlatformClaimGet {
-        platform: string,
+    platform: string,
     claims?: any,
     id: number,
     user_id: string
-    };
+};
 
 export interface IPostList {
-        id: number,
+    id: number,
     body: string,
     upvoted_count: number,
     subscription_level: string,
     user: IUserList[],
     comment_count: number,
     title: string
-    };
+};
 
 export interface IPostGet {
-        id: number,
+    id: number,
     subscription_level: string,
     body: string,
     upvoted_count: number,
     user: IUserList[],
     comment_count: number,
     title: string
-    };
+};
 
 export interface ISubscriberList {
-        subscription_id: number,
+    subscription_id: number,
     user_id: string,
     start_date: Date,
     due_date?: Date,
@@ -92,10 +92,10 @@ export interface ISubscriberList {
     subscription: ISubscriptionGet[],
     user: IUserList[],
     approved: boolean
-    };
+};
 
 export interface ISubscriberGet {
-        id: number,
+    id: number,
     subscription_id: number,
     start_date: Date,
     user_id: string,
@@ -105,85 +105,85 @@ export interface ISubscriberGet {
     subscription: ISubscriptionGet[],
     user: IUserList[],
     approved: boolean
-    };
+};
 
 export interface ISubscriberInsert {
     subscription_id: number,
     user_id: string,
     start_date: Date,
     approved: boolean
-    };
+};
 
 export interface ISubscriberUpdate {
-        id?: number,
+    id?: number,
     subscription_id?: number,
     approved?: boolean
-    };
+};
 
 export interface ISubscriptionList {
-        id: number,
+    id: number,
     user_id: string,
     name: string,
     cost: number,
     user: IUserList[]
-    };
+};
 
 export interface ISubscriptionGet {
-        id: number,
+    id: number,
     settings?: any,
     cost: number,
     name: string,
     user_id: string,
     user: IUserList[]
-    };
+};
 
 export interface ISubscriptionInsert {
-        name: string,
+    name: string,
     settings?: any,
     cost: number,
     user_id: string
-    };
+};
 
 export interface ISubscriptionUpdate {
-        name?: string,
+    name?: string,
     settings?: any,
     id?: number,
     cost?: number,
     user_id?: string
-    };
+};
 
 export interface ITradeList {
-    
-    };
+
+};
 
 export interface ITradeGet {
-    
-    };
+
+};
 
 export interface IUpvoteList {
-        post_id: string,
+    post_id: string,
     user_id: string,
     id: number
-    };
+};
 
 export interface IUpvoteGet {
-        id: number,
+    id: number,
     user_id: string,
     post_id: string
-    };
+};
 
 export interface IUserList {
-        id: string,
+    id: string,
     handle: string,
     tags: string[],
     display_name: string,
     profile_url?: string,
-    subscription: { cost:string, id:number , is_subscribed:boolean, count:number, settings:any, is_pending:boolean    },
+    subscription: { cost: string, id: number, is_subscribed: boolean, count: number, settings: any, is_pending: boolean },
     social_analytics?: any
-    };
+};
 
 export interface IUserGet {
-        handle: string,
+    handle: string,
     email: string,
     claims: IPlatformClaimList[],
     bio: string,
@@ -195,13 +195,13 @@ export interface IUserGet {
     profile_url?: string,
     banner_url?: string,
     analyst_profile?: Statics.IAnalystProfile,
-    subscription: { cost:string, id:number , is_subscribed:boolean, count:number, settings:any, is_pending:boolean    },
+    subscription: { cost: string, id: number, is_subscribed: boolean, count: number, settings: any, is_pending: boolean },
     settings?: Statics.IUserSettings,
     social_analytics?: any
-    };
+};
 
 export interface IUserUpdate {
-        id?: string,
+    id?: string,
     first_name?: string,
     last_name?: string,
     analyst_profile?: Statics.IAnalystProfile,
@@ -211,10 +211,10 @@ export interface IUserUpdate {
     banner_url?: string,
     bio?: string,
     social_analytics?: any
-    };
+};
 
 export interface IWatchlistList {
-        id: number,
+    id: number,
     name: string,
     note?: string,
     user: IUserList[],
@@ -222,10 +222,10 @@ export interface IWatchlistList {
     user_id?: string,
     item_count: number,
     saved_by_count: number
-    };
+};
 
 export interface IWatchlistGet {
-        user: IUserList[],
+    user: IUserList[],
     items: IWatchlistItemList[],
     note?: string,
     name: string,
@@ -233,50 +233,50 @@ export interface IWatchlistGet {
     type: string,
     saved_by_count: number,
     is_saved: boolean
-    };
+};
 
 export interface IWatchlistInsert {
-        name: string,
+    name: string,
     note?: string,
-    items: Omit<IWatchlistItemList,'watchlist_id'|'id'>[],
+    items: Omit<IWatchlistItemList, 'watchlist_id' | 'id'>[],
     type: string,
     user_id?: string
-    };
+};
 
 export interface IWatchlistUpdate {
-        id?: number,
+    id?: number,
     name?: string,
     note?: string,
-    items?: Omit<IWatchlistItemList,'watchlist_id'|'id'>[],
+    items?: Omit<IWatchlistItemList, 'watchlist_id' | 'id'>[],
     type?: string,
     user_id?: string
-    };
+};
 
 export interface IWatchlistItemList {
-        id: number,
+    id: number,
     symbol: string,
     watchlist_id: number,
     note?: string,
     date_added: Date
-    };
+};
 
 export interface IWatchlistItemGet {
-        id: number,
+    id: number,
     symbol: string,
     watchlist_id: number,
     note?: string,
     date_added: Date
-    };
+};
 
 export interface IWatchlistSavedList {
-        id: number,
+    id: number,
     user_id: string,
     watchlist_id: number
-    };
+};
 
 export interface IWatchlistSavedGet {
-    
-    };
+
+};
 
 
 export interface ISecurityPrices {
