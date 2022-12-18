9c855ddaf974642b532d8dc2ac109bb24035e29a
David Bozentka <djbozentka@gmail.com>
2022-12-18 08:43:24 -0500

    App.tsx fixed

--- app/vvv/TradingPostExpo/App.tsx
+++ app/vvv/TradingPostExpo/App.tsx
@@ -1,7 +1,7 @@
-import { configApi } from '@tradingpost/common/api/entities/static/EntityApiBase'
-import Constants, { AppOwnership } from 'expo-constants'
-import { useURL } from 'expo-linking'
-import { parse } from 'url'
+import {configApi} from '@tradingpost/common/api/entities/static/EntityApiBase'
+import Constants, {AppOwnership} from 'expo-constants'
+import {useURL} from 'expo-linking'
+import {parse} from 'url'
 
 if (!__DEV__) {
     configApi({
@@ -11,13 +11,13 @@ if (!__DEV__) {
 
     if (Constants.manifest?.hostUri) {
         configApi({
-            apiBaseUrl: `http://192.168.4.186:8082`//`http://${Constants.manifest?.hostUri?.split(":")[0]}:8082`
+            apiBaseUrl: `http://${Constants.manifest?.hostUri?.split(":")[0]}:8082`
         })
     } else {
 
         //manual ip for api server... have been trying to find a way to avoid this...
         configApi({
-            apiBaseUrl: `http://192.168.4.186:8082`//`http://${Constants.expoConfig?.extra?.localIp || "localhost"}:8082`
+            apiBaseUrl: `http://${Constants.expoConfig?.extra?.localIp || "localhost"}:8082`
         })
     }
 }
@@ -28,24 +28,24 @@ if (__DEV__ && Platform.OS === "web")
     })
 
 
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Button, IconRegistry } from '@ui-kitten/components';
+import {ApplicationProvider, Layout, Button, IconRegistry} from '@ui-kitten/components';
 import theme from "./theme-light.json"; // <-- Import app theme
-import { ToastProvider } from 'react-native-toast-notifications';
-import React, { useEffect, } from 'react';
+import {ToastProvider} from 'react-native-toast-notifications';
+import React, {useEffect,} from 'react';
 
 
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
 
-import { getSecurityList } from './SecurityList'
-import { Platform } from 'react-native'
+import {getSecurityList} from './SecurityList'
+import {Platform} from 'react-native'
 
 export default function App() {
-    const { isLoadingComplete } = useCachedResources();
+    const {isLoadingComplete} = useCachedResources();
     const colorScheme = useColorScheme();
 
     useEffect(() => {
@@ -67,11 +67,11 @@ export default function App() {
 
     }
 
-    return !isLoadingComplete ? null : <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
-        <IconRegistry icons={EvaIconsPack} />
+    return !isLoadingComplete ? null : <ApplicationProvider {...eva} theme={{...eva.light, ...theme}}>
+        <IconRegistry icons={EvaIconsPack}/>
         <ToastProvider>
-            <Navigation colorScheme={colorScheme} />
-            <StatusBar />
+            <Navigation colorScheme={colorScheme}/>
+            <StatusBar/>
         </ToastProvider>
     </ApplicationProvider>
 }
